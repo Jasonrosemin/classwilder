a342e393b652a0c661513862cbd98fea87c88cf3
rosemin <jasonliverpool@hotmail.fr>
2019-10-01 00:48:12 +0200

    classroom version

---
+++ Classroom.java
@@ -0,0 +1,14 @@
+public class Classroom {
+
+    public static void main(String[] args) {
+
+        
+        Wilder Franck  = new Wilder(" Franck ", true); 
+        // utilisation de getters      
+        System.out.println(Franck.WhoAmI());
+        
+        Wilder Kevin  = new Wilder (" Kevin ", false);  
+        System.out.println(Kevin.WhoAmI());
+        
+    }
+}
